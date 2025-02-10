a6cbe88dec7838992718247106af3d7014410597
samro <sam.rotthier@gmail.com>
2025-02-10 22:05:34 +0100

    Code Cleanup for Test.js

--- Test.js
+++ Test.js
@@ -1,40 +1,38 @@
 const { Console } = require("console")
-
 const apiKey = process.env.NEXT_PUBLIC_API_KEY
 
-/*
 //Ping site -> Response = { gecko_says: '(V3) To the Moon!' }
-const url = 'https://api.coingecko.com/api/v3/ping';
-const options = {method: 'GET', headers: {accept: 'application/json'}};
-fetch(url, options)
+const urlPing = 'https://api.coingecko.com/api/v3/ping';
+const optionsPing = {method: 'GET', headers: {accept: 'application/json'}};
+fetch(urlPing, optionsPing)
   .then(res => res.json())
   .then(json => console.log(json))
   .catch(err => console.error(err));
 
 
 //Ethereum Endpoint
-const url2 = 'https://api.coingecko.com/api/v3/simple/price?ids=Ethereum&vs_currencies=usd';
-const options2 = {
+const urlCoin = 'https://api.coingecko.com/api/v3/simple/price?ids=Ethereum&vs_currencies=usd';
+const optionsCoin = {
 method: 'GET',
     headers: {accept: 'application/json', 'x-cg-demo-api-key': apiKey}
   };
-  fetch(url2, options2)
+  fetch(urlCoin, optionsCoin)
     .then(res => res.json())
     .then(json => console.log(json))
     .catch(err => console.error(err));
 
+
 // Usable currencies
-const url3 = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies';
-const options3 = {
+const urlAllCurrenciesShort = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies';
+const optionsAllCurrenciesShort = {
   method: 'GET',
   headers: {accept: 'application/json', 'x-cg-demo-api-key': apiKey}
 };
-
-fetch(url3, options3)
+fetch(urlAllCurrenciesShort, optionsAllCurrenciesShort)
   .then(res => res.json())
   .then(json => console.log(json))
   .catch(err => console.error(err));
-*/
+
 
 //Fetching a certain coin with user input
 process.stdout.write('What coin would you like the price of? (example Ethereum, Bitcoin) -> ');
@@ -42,28 +40,26 @@ process.stdin.on('data', (data) => {
     const name = data.toString().trim().toLocaleLowerCase();
     console.log(`Chosen coin is, ${name}!`);
 
-    const url4 = `https://api.coingecko.com/api/v3/simple/price?ids=${name}&vs_currencies=usd`;
+    const urlCoinSearch = `https://api.coingecko.com/api/v3/simple/price?ids=${name}&vs_currencies=usd`;
     //console.log(url4)
-
-    const options4 = {
+    const optionsCoinSearch = {
         method: 'GET',
         headers: {
             accept: 'application/json', 
             'x-cg-demo-api-key': apiKey
         }
     };
-    
-    fetch(url4, options4)
+
+    fetch(urlCoinSearch, optionsCoinSearch)
     .then(res => res.json())
     .then(json => {
-        // Access the price properly from the response object
         const price = json[name]?.usd;
         if (price) {
             console.log(`The ${name} price = $${price}`);
         } else {
             console.log('Could not find price for this coin. Make sure you use the correct coin ID.');
         }
-        process.exit(); // Exit after getting the response
+        process.exit();
     })
     .catch(err => {
         console.error('Error:', err);
